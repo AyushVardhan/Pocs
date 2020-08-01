092b672a60316ca8f326f81e377449076f806348
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-08-01 23:36:58 +0530

    Ayush | Python script to move music files to dest

--- js-demo/utils.js
+++
@@ -1,8 +0,0 @@
-function showMessage(msg){
-    document.getElementById('message').textContent = msg;
-    // document.getElementById('message').innerHTML = msg;
-}
-
-function changePercentOff(percent){
-    document.getElementById('percent-off').textContent = percent + "% OFF";
-}
